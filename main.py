b3605e1041023b82fc4b58b41963b7223a000da1
Tommy <petrushov.artem@mail.ru>
2023-10-21 16:56:41 +0300

    first itteration

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot("5175169529:AAG-6i3kXmnDyRUd2sjyqB5zDvWTs4DGbI4")
 def start(message):
         mess = f"Привет, <b>{message.from_user.first_name}</b>👋 \n\nСамые вкусные сладости можно заказать по кнопке ниже😋👇"
         markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton('Заказать', web_app=WebAppInfo(url='https://heyartemno.github.io/webappmenu/')))
+        markup.add(types.InlineKeyboardButton('Заказать', web_app=WebAppInfo(url='https://heyartemno.github.io/testtelegramwebapp/')))
         bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
 
 bot.polling()
