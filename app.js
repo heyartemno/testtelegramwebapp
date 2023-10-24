9ae1193a30d407c3c5aae6e73f80991c477220e2
Tommy <petrushov.artem@mail.ru>
2023-10-24 23:52:35 +0300

    fix

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#2cab37";
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
 
-let item = "";
+let items = "";
 
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
