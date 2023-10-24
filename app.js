a0d4b03cb21dd8f3be8e1340389e298426f8bbd7
Tommy <petrushov.artem@mail.ru>
2023-10-24 20:23:15 +0300

    add app.js

---
+++ app.js
@@ -0,0 +1,85 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+
+let item = "";
+
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+
+btn1.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Товар добавлен в корзину!")
+        item = "1";
+        tg.MainButton.show();
+    }
+});
+
+btn2.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Товар добавлен в корзину!")
+        item = "2";
+        tg.MainButton.show();
+    }
+});
+
+btn3.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Товар добавлен в корзину!")
+        item = "3";
+        tg.MainButton.show();
+    }
+});
+
+btn4.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Товар добавлен в корзину!")
+        item = "4";
+        tg.MainButton.show();
+    }
+});
+
+btn5.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Товар добавлен в корзину!")
+        item = "5";
+        tg.MainButton.show();
+    }
+});
+
+btn6.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Товар добавлен в корзину!")
+        item = "6";
+        tg.MainButton.show();
+    }
+});
+
+Telegram.Webapp.onEvent("mainButtonClicked". function(){
+    tg.sendData(item);
+});
--- main.py
+++ main.py
@@ -2,7 +2,17 @@ import telebot
 from telebot import types
 from telebot.types import WebAppInfo
 
-bot = telebot.TeleBot("5175169529:AAG-6i3kXmnDyRUd2sjyqB5zDvWTs4DGbI4")
+bot = telebot.TeleBot("5175169529:AAFWRSHZGblFf5GNWdN3rMRDQF3iqRS11EE")
+PAYMENTS_TOKEN = "401643678:TEST:0363ce47-d7c7-47d4-8491-170838062f73"
+
+PRICE = {
+    '1': [types.LabeledPrice(label='Item1', amount=80)],
+    '2': [types.LabeledPrice(label='Item2', amount=100)],
+    '3': [types.LabeledPrice(label='Item3', amount=100)],
+    '4': [types.LabeledPrice(label='Item4', amount=100)],
+    '5': [types.LabeledPrice(label='Item5', amount=80)],
+    '6': [types.LabeledPrice(label='Item6', amount=100)]
+}
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -10,5 +20,25 @@ def start(message):
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton('Заказать', web_app=WebAppInfo(url='https://heyartemno.github.io/testtelegramwebapp/')))
         bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
+
+@bot.message_handler(content_types='web_app_data')
+def buy_process(web_app_message):
+    bot.send_invoice(web_app_message.chat.id,
+                           title='Laptop',
+                           description='Description',
+                           provider_token=PAYMENTS_TOKEN,
+                           currency='rub',
+                           need_email=True,
+                           prices=PRICE[f'{web_app_message.web_app_data.data}'],
+                           start_parameter='example',
+                           payload='some_invoice')
+
+@bot.pre_checkout_query_handler(lambda query: True)
+def pre_checkout_process(pre_checkout: types.PreCheckoutQuery):
+    bot.answer_pre_checkout_query(pre_checkout.id, ok=True)
+
+@bot.message_handler(content_types=['successful_payment'])
+def successful_payment(message: types.Message):
+    bot.send_message(message.chat.id, 'Заказ оформлен')
 
 bot.polling()
